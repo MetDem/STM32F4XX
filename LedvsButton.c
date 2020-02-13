643f3f0038d2b68807a3bfb01174ac7c36148953
Metin <40427636+MetDem@users.noreply.github.com>
2020-02-13 15:52:05 +0300

    Update LedvsButton.c

--- LedvsButton.c
+++ LedvsButton.c
@@ -5,9 +5,13 @@
 int main()
 {
     GPIO_InitTypeDef GPIO_InitStructure;                // GPIO_InitStructure is Value
-    
+    uint8_t button_input;
+    /*unsigned int */
+
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE); 
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); 
+
+    /* Clock Bus tanimlamalari */
 
 
     // LED CONGIRATION 
@@ -20,19 +24,35 @@ int main()
    
     // BUTTON CONFIGURATION
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;       
-    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_0            // kart mavi buton
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_0            // kart üzerindeki mavi buton A0-Pin
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;       //.._OD
     /* OD ya da PP olabilir external(harici-dışardan) direnç kullanılacaksa OD seçilir
     board üzerindeki buton PP ile kullanılır*/
-    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN       //.._DOWN  
-    /*PP olarak belirledikten sonra gerekirse Pull-Down ya da Pull-Up ayarlanabilinir*/
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN       //.._UP 
+    /* Butona basılmadığında 0 bigisi gidecektir */
+    /*PP olarak belirledikten sonra gerekirse Pull-Down ya da Pull-Up ayarlanabilinir.*/
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;  //.._50-25-2 MHz
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 
+    while(1)
+    {
+        button_input = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0);
 
-    /* Kullanılacak porta göre Clock Buslarını librarydan (.rcc) bakıp yazmak lazım */
-    /* GPIO_Init fonksiyonu olmadan yapılan ayarlamalar havada kalır */
+        if (button_input)
+        {
+            GPIO_SetBits(GPIOD, GPIO_Pin12 | GPIO_Pin13 | GPIO_Pin14 | GPIO_Pin15);
+        }
+        else
+        {
+            GPIO_ResetBits(GPIOD, GPIO_Pin12 | GPIO_Pin13 | GPIO_Pin14 | GPIO_Pin15);
+        }
+    }
+
+
+
+    /* Kullanılacak porta göre Clock Buslarını librarydan (.rcc) bakıp yazmak lazım*/
+    /* GPIO_Init fonksiyonu olmadan yapılan ayarlamalar havada kalır*/
 
 }
 
